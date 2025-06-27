14552ea1bb764df34149509b2027f32a0a7254fd
Chris <1905121+lauretano@users.noreply.github.com>
2025-06-27 14:19:48 -0400

    fix: remove more kernel packages from base image

--- build_files/t2-enablement.sh
+++ build_files/t2-enablement.sh
@@ -19,8 +19,8 @@ set -ouex pipefail
 # dnf5 -y copr disable ublue-os/
 
 dnf5 -y copr enable sharpenedblade/t2linux
-#dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-uki-virt kernel-modules-extra kernel-headers kernel-tools kernel-tools-libs
-dnf5 -y remove kernel-uki-virt kernel-tools kernel-tools-libs kernel-modules-extra kernel-headers
+dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-uki-virt kernel-modules-extra kernel-headers kernel-tools kernel-tools-libs
+#dnf5 -y remove kernel-uki-virt kernel-tools kernel-tools-libs kernel-modules-extra kernel-headers
 dnf5 -y versionlock delete kernel kernel-core kernel-modules \
   kernel-modules-core kernel-tools kernel-tools-libs
 #dnf5 -y --repo=copr:copr.fedorainfracloud.org:sharpenedblade:t2linux install kernel kernel-core kernel-modules kernel-modules-core kernel-tools kernel-tools-libs
