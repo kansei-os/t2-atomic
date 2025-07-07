9e3d85ae179371f499e509f8ec4a92adb5d74dc9
Chris Lauretano <1905121+lauretano@users.noreply.github.com>
2025-07-07 14:11:02 -0400

    feat: adding docker-ce and such to falcon variant

---
+++ build_files/apps-falcon.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install docker-ce and such
+
+dnf5 -y config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+dnf5 -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
