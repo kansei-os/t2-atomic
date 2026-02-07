2afea0d8d0c5d794c4b4e61614dc411be1203b59
Chris Lauretano <1905121+lauretano@users.noreply.github.com>
2026-02-07 17:37:34 -0500

    feat: kansei spin adding libsecret

--- build_files/apps-kansei.sh
+++ build_files/apps-kansei.sh
@@ -25,7 +25,8 @@ dnf5 -y install bolt cups-pk-helper firewalld firewall-config fuse \
   glibc-langpack-en plymouth-plugin-label plymouth-plugin-two-step \
   podman-compose podman-machine podman-tui powerstat cockpit-system \
   cockpit-ostree cockpit-selinux cockpit-networkmanager \
-  system-config-printer thermald zram-generator-defaults gnome-keyring-pam
+  system-config-printer thermald zram-generator-defaults gnome-keyring-pam \
+  gnome-keyring libsecret
 
 #### wayland wm environment
 dnf5 -y install tuned tuned-ppd xorg-x11-server-Xwayland \
